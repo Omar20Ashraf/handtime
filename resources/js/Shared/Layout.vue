48054e64e5780ffdfbe4a717652729cd5aae10e8
Omar20Ashraf <omaarashraf2020@gmail.com>
2023-09-12 05:26:15 +0300

    make the layout files

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <slot />
+    </div>
+
+    <Footer />
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+export default {
+    components: {
+        Footer,
+    },
+};
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,19 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Meta -->
+    <link rel="icon" href="{{ asset('style') }}/images/fevicon/fevicon.png" type="image/gif" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>HandTime</title>
 
-        @vite('resources/js/app.js')
-        @inertiaHead
-    </head>
-    <body>
-        @inertia
-    </body>
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('style') }}/css/bootstrap.css" />
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+    <!-- font awesome style -->
+    <link href="{{ asset('style') }}/css/font-awesome.min.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('style') }}/css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="{{ asset('style') }}/css/responsive.css" rel="stylesheet" />
+
+    @vite('resources/js/app.js')
+    @inertiaHead
+
+</head>
+
+<body>
+    @inertia
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="{{ asset('style') }}/js/jquery-3.4.1.min.js"></script>
+    <!-- popper js -->
+    {{-- <script type="text/javascript" src="{{ asset('style') }}/js/popper.min.js"></script> --}}
+    <!-- bootstrap js -->
+    <script type="text/javascript" src="{{ asset('style') }}/js/bootstrap.js"></script>
+    <!-- custom js -->
+    <script type="text/javascript" src="{{ asset('style') }}/js/custom.js"></script>
+
+
+</body>
+
 </html>
